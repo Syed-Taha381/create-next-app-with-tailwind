190ff2173544ec7e166f43366a99164d3f1bbccf
Syed-Taha381 <staha381@gmail.com>
2022-05-10 12:14:30 +0500

    origin

---
+++ pages/checking-tailwind.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const checkingTailwind = () => {
+  return (
+    <div className="text-3xl font-bold underline">checkingTailwind</div>
+  )
+}
+
+export default checkingTailwind
